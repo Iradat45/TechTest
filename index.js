6a43844446a5cb7e52aa58213a1093cc8b26e88b
Iradat <iradati@hotmail.com>
2023-08-07 13:30:54 +0100

    Created for loop to display the JSON data into HTML table

--- index.js
+++ index.js
@@ -15,6 +15,35 @@ fetch("./students.json")
 function ArrangeData(data) {
   const students = data;
   students.sort(CompareStudentID);
+
+  for (let j = 0; j < students.length; j++) {
+    let newTBody = document.createElement("tbody");
+    let newRow = document.createElement("tr");
+    let cell;
+
+    for (let i = 0; i < 4; i++) {
+      cell = document.createElement("td");
+      cell.rowSpan = 5;
+      cell.innerHTML = students[j][headers[i]];
+      newRow.appendChild(cell);
+      newTBody.appendChild(newRow);
+    }
+
+    for (let i = 0; i < 5; i++) {
+      cell = document.createElement("td");
+      cell.innerHTML = students[j].grades.subjects[i].name;
+      newRow.appendChild(cell);
+      cell = document.createElement("td");
+      cell.innerHTML = students[j].grades.subjects[i].score;
+      newRow.appendChild(cell);
+      cell = document.createElement("td");
+      cell.innerHTML = students[j].attendance.subjects[i].score;
+      newRow.appendChild(cell);
+      newTBody.appendChild(newRow);
+      myTable.appendChild(newTBody);
+      newRow = document.createElement("tr");
+    }
+  }
 }
 
 function CompareStudentID(a, b) {
